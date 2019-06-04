604127e7df732e28f20320cd549a281b33e9c488
xinxin Fly <544416035@qq.com>
2019-06-04 20:25:19 +0800

    city router city header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,11 +5,14 @@
     </div>
     <div class="header-input">
         <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
     </div>
+    <router-link to="/city">
     <div class="header-right">
         {{this.city}}
         <span class="iconfont arrow-icon">&#xe6aa;</span>
     </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,7 +30,7 @@ export default {
     @import '~styles/varibles.styl'
     .header
         display flex
-        line-height .86rem
+        line-height $headerHeight 
         background $bgColor
         color #fff
         .header-left
@@ -50,6 +53,7 @@ export default {
             width 1.24rem
             float right
             text-align center
+            color #fff
             .arrow-icon
                 margin-left -.04rem
                 font-size .24rem
